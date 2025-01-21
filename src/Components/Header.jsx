78b73cf8e632c6aabd4017ecec07a0f33b74c97e
Satish Timalsina <timalsinasatish430@gmail.com>
2025-01-21 12:47:41 +0545

    added functionality

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,103 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "../App.css";
 import { ThemeContext } from "../Hooks/Context";
 
 function Header() {
   const { isDarkTheme, toggleTheme } = useContext(ThemeContext);
+  const [checkedTasks, setCheckedTasks] = useState({});
+  const [newtask, setNewtask] = useState("");
+  const [status, setStatus] = useState("all");
+
+// static lists of tasks 
+  const [tasks, setTasks] = useState([
+    { id: 1, label: "Jog around", status: "active" },
+    { id: 2, label: "Meditation", status: "active" },
+    { id: 3, label: "Play football", status: "active" },
+    { id: 4, label: "Singing song", status: "active" },
+    { id: 5, label: "Play cricket", status: "active" },
+    { id: 6, label: "Run", status: "active" },
+    { id: 7, label: "Read Books", status: "active" },
+  ]);
+  // for  showing the tasks based on the status
+  const filteredTasks =
+    status === "all"
+      ? tasks
+      : status === "active"
+      ? tasks.filter((task) => !checkedTasks[task.id])
+      : tasks.filter((task) => checkedTasks[task.id]);
+// for handling the checkbox change
+  const handleCheckboxChange = (taskId) => {
+    setCheckedTasks((prev) => ({
+      ...prev,
+      [taskId]: !prev[taskId],
+    }));
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId
+          ? { ...task, status: checkedTasks[taskId] ? "active" : "completed" }
+          : task
+      )
+    );
+  };
+// for adding the task
+  const AddTask = (e) => {
+    if (e.key === "Enter") {
+      if (checkedTasks["new"]) {
+        const newTask = {
+          id: Date.now(),
+          label: newtask,
+          status: "active",
+        };
+
+        setTasks((prevTasks) => [...prevTasks, newTask]);
+
+        setCheckedTasks((prev) => ({
+          ...prev,
+          [newTask.id]: false,
+        }));
+        setNewtask("");
+      }
+    }
+  };
+// for clearing the completed tasks
+  const clearCompletedTasks = () => {
+    const remainingTasks = tasks.filter((task) => !checkedTasks[task.id]);
+    setTasks(remainingTasks);
+    setCheckedTasks({});
+  };
+// for handling the status of the tasks
+  const deleteTask = (id) => {
+    const filteredTask = tasks.filter((task) => task.id !== id);
+    setTasks(filteredTask);
+  };
+  const taskCounts = {
+    all: tasks.length,
+    active: tasks.filter((task) => task.status === "active").length,
+    completed: tasks.filter((task) => task.status === "completed").length,
+  };
+  // for changing the status of the tasks
+  const taskStatus = ["All", "Active", "Completed"];
+    // for changing the theme of the app
+  const appStyle = isDarkTheme
+    ? {
+        backgroundImage: "url('src/assets/bg-desktop-dark.jpg')",
+        backgroundColor: "black",
+        backgroundRepeat: "no-repeat",
+        color: "white",
+      }
+    : {
+        backgroundImage: "url('src/assets/bg-desktop-light.jpg')",
+        backgroundRepeat: "no-repeat",
+        color: "black",
+      };
+        // for changing the icon of the theme conditionally
+  const iconClass = isDarkTheme ? "fa-solid fa-moon" : "fa-solid fa-sun";
+      // for changing the card style conditionally
+  const cardStyle = isDarkTheme
+    ? "bg-gray-700 text-white"
+    : "bg-white text-black";
+
   const data = (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -20,45 +113,18 @@ function Header() {
     </svg>
   );
 
-  const tasks = [
-    { id: "jog", label: "Jog around" },
-    { id: "meditation", label: "Meditation" },
-    { id: "football", label: "Play football" },
-    { id: "song", label: "singing song " },
-    { id: "play", label: "Play cricket " },
-    { id: "run", label: "Run" },
-    { id: "books", label: "Read Books" },
-  ];
-
-  const taskStatus = ["All", "Active", "Completed"];
-  const appStyle = isDarkTheme
-    ? {
-        backgroundImage: "url('src/assets/bg-desktop-dark.jpg')",
-        backgroundColor: "black",
-        backgroundRepeat: "no-repeat",
-        color: "white",
-      }
-    : {
-        backgroundImage: "url('src/assets/bg-desktop-light.jpg')",
-        backgroundRepeat: "no-repeat",
-        color: "black",
-      };
-  const iconClass = isDarkTheme ? "fa-solid fa-moon" : "fa-solid fa-sun";
-
-  const cardStyle = isDarkTheme
-    ? "bg-gray-700 text-white"
-    : "bg-white text-black";
-
   return (
     <>
+        {/* {// for the main div} */}
       <div
         className={`h-[100vh] ${
           isDarkTheme ? "bg-gray-800 text-white" : "bg-white text-black"
         }`}
       >
+        {/* {// for the header} */}
         <div
           style={appStyle}
-          className="relative w-full h-[calc(100vh-40%)] bg-cover bg-center"
+          className="relative w-full h-[calc(100vh-60%)] bg-cover bg-center"
         >
           <header className="absolute top-0 left-0 w-full p-6">
             <div className="flex justify-between items-center max-w-4xl mx-auto">
@@ -72,80 +138,145 @@ function Header() {
             </div>
           </header>
         </div>
-
-        <div className={`px-4 py-4  ${cardStyle}`}>
-
+        {/* {// for the main div} */}
+        <div className={`px-4 py-4 ${cardStyle}`}>
           <div className="relative top-[-150px] mx-auto w-full max-w-lg">
-            <div className={`rounded-lg mb-2 p-1 ${cardStyle}`}>
+          {/* {// for the input field} */}
+            <div
+              className={`rounded-lg mb-2 p-1 flex items-center ${cardStyle}`}
+            >
+              <div
+                className={`ml-4 flex items-center justify-center border w-6 h-6 rounded-full cursor-pointer ${
+                  checkedTasks["new"]
+                    ? "bg-blue-500 border-blue-500"
+                    : "bg-white border-gray-300"
+                }`}
+                onClick={() => handleCheckboxChange("new")}
+                role="checkbox"
+                aria-checked={checkedTasks["new"]}
+              >
+                {checkedTasks["new"] && (
+                  <i className="fa-solid fa-check text-white"></i>
+                )}
+              </div>
+
               <input
                 type="text"
                 placeholder="Create a new todo..."
                 className={`w-full p-3 rounded focus:outline-none ${cardStyle}`}
+                value={newtask}
+                onChange={(e) => setNewtask(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && newtask.trim() !== "") {
+                    AddTask(e);
+                  }
+                }}
               />
             </div>
           </div>
-
+          {/* {// for the list of tasks} */}
           <section className="relative top-[-100px] mx-auto w-full max-w-lg">
             <div className={`p-6 rounded-lg shadow-lg ${cardStyle}`}>
-              <ul className="space-y-4 max-h-[300px] overflow-y-auto">
-                {tasks.map((task, index) => (
+              <ul className="space-y-4 max-h-[300px] overflow-y-scroll lg:space-y-4 scrollbar-thin scrollbar-thumb-rounded">
+                {filteredTasks.map((task) => (
                   <li
-                    key={index}
+                    key={task.id}
                     className="flex items-center justify-between border-b border-gray-600 pb-2"
                   >
                     <div className="flex items-center">
-                      <input
-                        type="checkbox"
-                        id={task.id}
-                        className="mr-2 cursor-pointer"
-                      />
-                      <label htmlFor={task.id} className="cursor-pointer p-1">
+                      <div
+                        className={`flex items-center justify-center border border-gray-500 w-6 h-6 rounded-full cursor-pointer ${
+                          checkedTasks[task.id]
+                            ? "bg-blue-500 border-blue-500 "
+                            : "bg-white border-gray-00"
+                        }`}
+                        onClick={() => handleCheckboxChange(task.id)}
+                        role="checkbox"
+                        aria-checked={checkedTasks[task.id]}
+                      >
+                        {checkedTasks[task.id] && (
+                          <i className="fa-solid fa-check rounded-full text-white"></i>
+                        )}
+                      </div>
+                      <label
+                        htmlFor={task.id}
+                        className={`cursor-pointer p-1 ml-2 ${
+                          checkedTasks[task.id]
+                            ? "line-through text-gray-500"
+                            : "border-gray-200"
+                        }`}
+                      >
                         {task.label}
                       </label>
                     </div>
-                    <button className="group">{data}</button>
+                    <button
+                      onClick={() => deleteTask(task.id)}
+                      className="group"
+                    >
+                      {data}
+                    </button>
                   </li>
                 ))}
               </ul>
-
+                {/* // for the footer */}
               <div className="lg:flex lg:items-center lg:justify-between mt-4 flex items-center justify-between">
                 <div className="lg:order-1">
-                  <p className="text-sm">3 items left</p>
+                  <p className="text-sm">
+                    <span className="p-1 font-semibold">
+                      {taskCounts[status]}
+                    </span>
+                    items left
+                  </p>
                 </div>
-                <div className="flex justify-center space-x-4 p-2 sm:shadow-sm lg:order-2 ">
-                  {taskStatus.map((status, index) => (
+                <div className="flex justify-center space-x-4 p-2 sm:shadow-sm lg:order-2">
+                  {taskStatus.map((value) => (
                     <button
-                      key={index}
-                      className="text-blue-500 text-sm hover:font-semibold hidden lg:block"
+                      key={value}
+                      onClick={() => {
+                        setStatus(value.toLocaleLowerCase());
+                      }}
+                      className={` text-sm hover:font-semibold hidden lg:block   ${
+                        status === value.toLocaleLowerCase()
+                          ? "font-semibold text-blue-500"
+                          : "text-black-50"
+                      }`}
                     >
-                      {status}
+                      {value}
                     </button>
                   ))}
                 </div>
                 <div className="lg:order-3">
-                  <button className="text-blue-500 text-sm hover:font-semibold">
+                  <button
+                    onClick={clearCompletedTasks}
+                    className="text-blue-500 text-sm hover:font-semibold"
+                  >
                     Clear Completed
                   </button>
                 </div>
               </div>
-                
-           
             </div>
             <div>
-                  <div className="flex justify-center space-x-4 p-2 shadow   items-center mt-2 lg:order-2 lg:hidden">
-                  {taskStatus.map((status, index) => (
-                    <button
-                      key={index}
-                      className="text-blue-500 text-sm hover:font-semibold"
-                    >
-                      {status}
-                    </button>
-                  ))}
-                </div>
-                  </div>
+              <div className="flex justify-center space-x-4 p-2 shadow items-center mt-2 lg:order-2 lg:hidden">
+                {taskStatus.map((value) => (
+                  <button
+                    key={value}
+                    onClick={() => {
+                      setStatus(value.toLocaleLowerCase());
+                    }}
+                    className={` text-sm hover:font-semibold   ${
+                      status === value.toLocaleLowerCase()
+                        ? "font-semibold text-blue-500"
+                        : "text-black-50"
+                    }`}
+                  >
+                    {value}
+                  </button>
+                ))}
+              </div>
+            </div>
             <p className="mt-4 text-center text-sm">
-                Drag and Drop to reorder list
-              </p>
+              Drag and Drop to reorder list
+            </p>
           </section>
         </div>
       </div>
